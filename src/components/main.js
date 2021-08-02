dff1351825a647b16afc2ba75738e285d6d1a2c1
Mamoun Hussein <mamoon.husen1006@gmail.com>
2021-08-02 18:35:50 +0300

    solved lab02

--- src/components/main.js
+++ src/components/main.js
@@ -1,48 +1,26 @@
 import React, { Component } from "react";
 import HornedBeast from "./hornedbeast";
+import { Row, Col } from "react-bootstrap";
+import data from "../data/data.json";
+
 export default class Main extends Component {
   render() {
-    let arr = [
-      {
-        image_url:
-          "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-        title: "UniWhal",
-        description: "A unicorn and a narwhal nuzzling their horns",
-        keyword: "narwhal",
-        horns: 1,
-      },
-      {
-        image_url:
-          "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80",
-        title: "Rhino Family",
-        description: "Mother (or father) rhino with two babies",
-        keyword: "rhino",
-        horns: 2,
-      },
-      {
-        image_url:
-          "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg",
-        title: "Unicorn Head",
-        description: "Someone wearing a creepy unicorn head mask",
-        keyword: "unicorn",
-        horns: 1,
-      },
-    ];
-
     return (
       <main>
-        {arr.map((e, i) => {
-          return (
-            <div>
-              <HornedBeast
-                src={e.image_url}
-                title={e.title}
-                description={e.description}
-                key={e.keyword}
-              />
-            </div>
-          );
-        })}
+        <Row xs={1} lg={4} className={"g-1"} md={3} sm={2}>
+          {data.map((e, i) => {
+            return (
+              <Col className={"g-4"}>
+                <HornedBeast
+                  src={e.image_url}
+                  title={e.title}
+                  description={e.description}
+                  key={e.keyword}
+                />
+              </Col>
+            );
+          })}
+        </Row>
       </main>
     );
   }
