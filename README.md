a78caede9072bd559d22b43f89011d0eec5ec79e
Mamoun Hussein <mamoon.husen1006@gmail.com>
2021-08-02 18:40:29 +0300

    added the readme file and added small string in footer

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ Estimate of time needed to complete: 2 Hours
 
 Start time: 14:50
 
-Finish time: **\_**
+Finish time: around 16:00
 
-Actual time needed to complete: **\_**
+Actual time needed to complete: almost 1 hour (i finished late than 16:00 beacuse i took a lot of breaks between )
--- src/components/footer.js
+++ src/components/footer.js
@@ -21,12 +21,13 @@ export default class Footer extends Component {
             ? "url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='32' height='32' viewport='0 0 100 100' style='fill:black;font-size:24px;'><text y='50%'>⚔️</text></svg>\") 16 0, auto"
             : "auto",
         }}
-        onClick={() => {
+        onClick={(e) => {
           if (this.props.hero) {
             this.setState({ count: this.state.count + 1 });
           }
           if (this.state.count >= 10) {
             this.props.change();
+            e.target.style.cursor = "auto";
           }
         }}
       >
